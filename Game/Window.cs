e2df1a606de2d806c21d62a8f0f1b15fc7aa794e
IsaelGabriel <i234258@dac.unicamp.br>
2025-11-01 18:05:05 -0300

    Add separate window class

---
+++ Game/Window.cs
@@ -0,0 +1,36 @@
+using Raylib_cs;
+
+namespace halloween.Game;
+
+public static class Window
+{
+    #region SetupInfo
+
+    private readonly static string Title = "Jam-O'-Ween";
+    const int WIDTH = 600;
+    const int HEIGHT = 600;
+
+    #endregion
+
+
+    public static void InitWindow()
+    {
+        Raylib.InitWindow(WIDTH, HEIGHT, Title);
+
+        while(!Raylib.WindowShouldClose())
+        {
+            Raylib.ClearBackground(Color.Beige);
+            Raylib.BeginDrawing();
+            {
+                Raylib.DrawFPS(0, 0);
+                
+            }
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,9 @@
 ﻿using Raylib_cs;
 
-public static class Game
+public static class Program
 {
-#region WindowSetupInfo
-    private readonly static string Title = "Jam-O'-Ween";
-    const int WIDTH = 600;
-    const int HEIGHT = 600;
-#endregion
-
-
     static void Main()
     {
-        Raylib.InitWindow(WIDTH, HEIGHT, Title);
-
-        while(!Raylib.WindowShouldClose())
-        {
-            Raylib.ClearBackground(Color.Beige);
-            Raylib.BeginDrawing();
-            {
-                Raylib.DrawFPS(0, 0);
-                
-            }
-            Raylib.EndDrawing();
-        }
-
-        Raylib.CloseWindow();
+        
     }
 }
